ff4977f90815ffcd5c987114f8bfb6fae88357fa
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-02-22 09:13:39 +0700

    first commit

---
+++ Produk/bambi-diaper-rash-cream/share.js
@@ -0,0 +1,17 @@
+// share wa
+var element = document.getElementById("Whatsapp");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://wa.me/?text=Bambi Diaper Rash Cream | Glamary https://www.glamary.store/Produk/bambi-diaper-rash-cream/';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// share wa
+
+// share telegram
+var element = document.getElementById("Telegram");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://telegram.me/share/url?text=Bambi Diaper Rash Cream | Glamary Image&url=https://www.glamary.store/Produk/bambi-diaper-rash-cream/';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// share telegram
