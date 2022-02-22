2e52092d62c4c060074f62ffc2278a0a1ec2c6c9
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-02-22 09:14:18 +0700

    first commit

--- Produk/bambi-baby-telon-balm/share.js
+++ Produk/bambi-baby-telon-balm/share.js
@@ -2,7 +2,7 @@
 var element = document.getElementById("Whatsapp");
 var parent = element.parentNode;
 var link = document.createElement('a');
-link.href = 'https://wa.me/?text=Bambi Bambi Baby Telon Balm 50ml | Glamary https://www.glamary.store/Produk/bambi-baby-telon-balm/';
+link.href = 'https://wa.me/?text=Bambi Baby Telon Balm 50ml | Glamary https://www.glamary.store/Produk/bambi-baby-telon-balm/';
 link.appendChild(element.cloneNode(true));
 parent.replaceChild(link, element);
 // share wa
@@ -11,7 +11,7 @@ parent.replaceChild(link, element);
 var element = document.getElementById("Telegram");
 var parent = element.parentNode;
 var link = document.createElement('a');
-link.href = 'https://telegram.me/share/url?text=Bambi Bambi Baby Telon Balm 50ml | Glamary Image&url=https://www.glamary.store/Produk/bambi-baby-telon-balm/';
+link.href = 'https://telegram.me/share/url?text=Bambi Baby Telon Balm 50ml | Glamary Image&url=https://www.glamary.store/Produk/bambi-baby-telon-balm/';
 link.appendChild(element.cloneNode(true));
 parent.replaceChild(link, element);
 // share telegram
