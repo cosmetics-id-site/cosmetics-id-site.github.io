b1d1ca66de369158470dbf18a052fb1f0d50b5d7
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-02-25 08:16:04 +0700

    first commit

---
+++ Produk/bambi-baby-liquid-cleanser/share.js
@@ -0,0 +1,17 @@
+// share wa
+var element = document.getElementById("Whatsapp");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://wa.me/?text=Bambi Baby Liquid Cleanser 450ml | Glamary https://www.glamary.store/Produk/bambi-baby-liquid-cleanser/';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// share wa
+
+// share telegram
+var element = document.getElementById("Telegram");
+var parent = element.parentNode;
+var link = document.createElement('a');
+link.href = 'https://telegram.me/share/url?text=Bambi Baby Liquid Cleanser 450ml | Glamary Image&url=https://www.glamary.store/Produk/bambi-baby-liquid-cleanser/';
+link.appendChild(element.cloneNode(true));
+parent.replaceChild(link, element);
+// share telegram
